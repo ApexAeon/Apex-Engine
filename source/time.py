c130234878acec5e2d79f45400b9b3e8ecd1ea62
Apex Aeon <jlapex.aeon@gmail.com>
2018-04-24 14:43:05 -0500

    boi
    boi

--- source/time.py
+++ source/time.py
@@ -1,2 +1,21 @@
-def get()
-def log()
+continuum = {}
+indicies = []
+def retrieve(index):
+  while True:
+    
+def capture(state, index):
+  if len(indicies) is 0: # If this is the first capture, place the entire state into the continuum.
+    continuum[index] = state
+  else: # If this is not the first capture, place the difference between the previous state and the new state into the continuum.
+    past = {} 
+    for change in indicies: # Compile all the past state changes leading up to but excluding the present state.
+      past.update(continuum[change])
+    for key in past: # Compare the past and the present.
+      if state[key] != past[key]: # If they differ
+        continuum[index].update({key:state[key]}) # Update the continuum
+    indicies.append(index) # Add the time index to the indicies list.
+      
+
+  
+   
+
